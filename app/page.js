2a602dcdb95ec0224603b23bd8dd45ee56c73a36
NithinKonda <nithinkonda142@gmail.com>
2024-12-10 17:53:02 +0530

    add firestore

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import React from "react";
 import SignIn from "./components/SignIn";
+import ClickCounter from "./components/ClickCounter";
 
 export default function Home() {
   return (
     <div>
       <SignIn />
+      <ClickCounter />
     </div>
   );
 }
--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,21 @@
 // firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 // Paste your Firebase configuration here
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id",
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth };
+export { auth, db };
