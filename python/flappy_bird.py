289c950259b8cdd379af877835c9791c6f3ed253
NithinKonda <nithinkonda142@gmail.com>
2025-03-14 23:18:26 +0530

    feat: Implement FlappyBird class with game initialization

--- python/flappy_bird.py
+++ python/flappy_bird.py
@@ -1,3 +1,23 @@
 import curses
 import random
 import time
+
+
+class FlappyBird:
+    def __init__(self, screen):
+        self.screen = screen
+        curses.curs_set(0)  
+        self.screen.timeout(0)
+        
+        self.height, self.width = self.screen.getmaxyx()
+        
+        self.bird_char = ">"
+        self.obstacle_char = "|"
+        self.gap_size = 6
+        self.gravity = 0.5  
+        self.flap_power = -2  
+        
+        self.reset_game()
+
+
+        
