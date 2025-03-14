22b22104bea535aa80780c6f7e7c684355dabf65
NithinKonda <nithinkonda142@gmail.com>
2025-03-14 23:18:46 +0530

    feat: Add reset_game method to initialize game state

--- python/flappy_bird.py
+++ python/flappy_bird.py
@@ -20,4 +20,15 @@ class FlappyBird:
         self.reset_game()
 
 
+    def reset_game(self):
+
+        self.bird_x = self.width // 4
+        self.bird_y = self.height // 2
+        self.bird_velocity = 0
         
+        self.obstacles = []
+        self.new_obstacle()
+        
+        self.score = 0
+        self.game_over = False
+        self.last_update = time.time()
