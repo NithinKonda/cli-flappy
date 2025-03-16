2b124e32819ed231f733c4ca084b36b1928c4566
NithinKonda <nithinkonda142@gmail.com>
2025-03-16 23:15:07 +0530

    feat: Define GameState structure to manage game variables and state in FlappyBird

--- c/flappy_bird.c
+++ c/flappy_bird.c
@@ -13,3 +13,25 @@ typedef struct {
     int gap_end;
     int passed;
 } Obstacle;
+
+typedef struct {
+    int width;
+    int height;
+    int bird_x;
+    float bird_y;
+    float bird_velocity;
+    char* bird_chars[3];
+    int bird_frame;
+    const char* obstacle_char;
+    const char* obstacle_top;
+    const char* obstacle_bottom;
+    Obstacle obstacles[MAX_OBSTACLES];
+    int num_obstacles;
+    int gap_size;
+    float gravity;
+    float flap_power;
+    int score;
+    int game_over;
+    struct timespec last_update;
+    int animation_counter;
+} GameState;
